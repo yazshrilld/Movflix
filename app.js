599cec1d1b288d8ddf0c8f032c2ed7dfd872c359
yazshrilld <yazshrilld@gmail.com>
2021-08-04 18:52:58 +0100

    javascript codes

--- app.js
+++ app.js
@@ -9,9 +9,21 @@
     // using the arrow class
 
     arrows_class.forEach((arrow,i)=>{ // the forEach is targeting individual element of the arrow class
+        const itemNumber = movieList_class[i].querySelectorAll("img").length;
+        console.log(itemNumber);
+        let clickingCount = 0;
         arrow.addEventListener("click", ()=> {
-            // console.log("you clicked the :" + i + " row arrow" );   THIS CHECKS THE ARROW THAT WAS CLICKED
-            movieList_class[i].style.backgroundColor = "blue";
+            clickingCount++;
+            if( itemNumber - (3 + clickingCount) >= 0 ) {
+                 // console.log("you clicked the :" + i + " row arrow" );   THIS CHECKS THE ARROW THAT WAS CLICKED
+                movieList_class[i].style.transform = `translateX(${
+                movieList_class[i].computedStyleMap().get("transform")[0].x.value-300
+            }px)`;
+            }
+            else {
+                movieList_class[i].style.transform = "translateX(0)";
+                clickingCount = 0;
+            }
         })
         //Next is to find initial position of div after every click
     }) 
